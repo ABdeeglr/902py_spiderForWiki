fee1f862cab3da30a0a0854d26aea85c522a77a4
ABdeeglr <abdeeglr@icloud.com>
2023-06-01 15:53:43 +0800

    获取连接、清洗连接、下载图片的逻辑测试成功

--- main.py
+++ main.py
@@ -3,18 +3,37 @@ import thislib.mom as mom
 
 
 if __name__ == "__main__":
+
+    # 信息部分
     url = "https://azurlane.koumakan.jp/wiki/Centaur/Gallery"
+    url_2 = "https://azurlane.koumakan.jp/wiki/Br%C3%BCnhilde/Gallery"
     fpath = "./data/html/Centaur.html"
+    dpath = "./data/html/Brunhilde.html"
+
 
     used_headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.57"
     }
 
-    luca.catcher_HTMLCatcher(url, fpath, used_headers)
+    # 获取网页资源
+    luca.catcher_HTMLCatcher(url_2, dpath, used_headers)
 
+    # 获取图片链接列表
     used_list = []
+    mom.URLFileCatcher(dpath, used_list, mom.filter_cursiveURLCatcher)
+    used_list = mom.cleanerTotally(used_list)
+    
+    # 图片链接存储
+    with open("demo2.txt", "w", encoding="utf-8") as wf:
+        for line in used_list:
+            wf.write(line)
+            wf.write("\n")
 
-    mom.URLFileCatcher(fpath, used_list, mom.filter_cursiveURLCatcher)
+    # 图片下载
 
     for line in used_list:
-        print(line)
+        if "Bg_20" in line:
+            continue
+        else:
+            url = "https://{}".format(line)
+            luca.downloader_singlePictureDownload(url, dir_path="./data/pic_demo", used_headers=used_headers)
--- thislib/mom.py
+++ thislib/mom.py
@@ -3,6 +3,15 @@
 # 2. Find the certain url in HTML text
 # 3. Clean the url and store it in JSON format
 
+import json
+
+json_dir = "./data/json/"
+
+#  _____________________
+# |                    |
+# |    filter part     |
+# |____________________|
+
 
 def rEE_filter01(line:str)->tuple:
     """
@@ -61,7 +70,98 @@ def URLFileCatcher(file_path:str, dump_list:list, filter)->None:
                 singleLineURLCatcher(line, dump_list, filter)
 
 
+#  _____________________
+# |                    |
+# |    cleaner part    |
+# |____________________|
+
 # def cleaner_baseURLCleaner(url_list:list, )
+
+
+def cleaner_deleteSpaceOfList(lst):
+    """
+    删除 URL_list 中空值, 换行符, 重复项
+    """
+    processed_list = []
+
+    lst2 = []
+
+    for item in lst:
+        if item not in lst2:
+            lst2.append(item)
+
+    for item in lst2:
+        # 去除字符串两端的空格
+        item = item.strip()
+
+        # 检查字符串是否为空
+        if item:
+            # 去除字符串末尾的换行符
+            item = item.rstrip('\n')
+
+            # 将处理后的字符串添加到结果列表
+            processed_list.append(item)
+
+    return processed_list
+
+
+
+def cleaner_deleteThubmPictures(lst):
+    """
+    根据 URL 删除多余的图片链接
+    """
+    
+    lst2 = []
+
+    # 模糊删除
+    for item in lst:
+        # 去除缩略图和 Q 版图
+        if item.lower().find("chibi") > 0:
+            continue
+        elif "skin_bg" in item.lower():
+            continue
+        elif "icon" in item.lower():
+            continue
+        else:
+            lst2.append(item.replace("thumb/", ""))
+
+    processed_list = []
+
+    # 精确删除
+    for item in lst2:
+        if item.find("MainDayBG") > 0:
+            continue
+        if item.find("Ruby") > 0:
+            continue
+        if item.find("Banner") > 0:
+            continue
+        else:
+            processed_list.append(item)
+
+    return processed_list
+
+
+def cleanerTotally(lst):
+    lst = cleaner_deleteThubmPictures(lst)
+    lst = cleaner_deleteSpaceOfList(lst)
+
+    return lst
+
+#  _____________________
+# |                    |
+# |   switcher part    |
+# |____________________|
+
+def loader_saveListToJsonFile(url_list:list, filename):
+    # 分析列表并存储为字典
+
+    json_data = json.dumps(lst)
+
+    # 写入 JSON 数据到文件
+    with open(filename, 'w') as file:
+        file.write(json_data)
+
+
 
 
 
